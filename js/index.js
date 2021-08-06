d4e27dd87b49163a5d307b361fd2e6354cb271dc
Julio Alcantara <jalcantara88@gmail.com>
2021-08-05 22:43:41 -0700

    transfer of files from Jalcantara88 repo

---
+++ js/index.js
@@ -0,0 +1,31 @@
+
+
+function aHover(element) {
+    element.setAttribute("src", "assets/atlantisidle.gif");
+    console.log("hovering");
+}
+
+function dHover(element) {
+    element.setAttribute("src", "assets/discordidle.gif");
+    console.log("hovering");
+}
+
+function tHover(element) {
+    element.setAttribute("src", "assets/twitteridle.gif");
+    console.log("hovering");
+}
+
+function lightOff(element) {
+    element.setAttribute("src", "assets/empty.png");
+}
+
+/*
+<a 
+                    href="https://docs.google.com/presentation/d/1uOcYKPrBEqKTxaufYiDkfIpILrzrLEOcMVSWpq1H1oc/edit#slide=id.ge1d4042eb9_3_1" 
+                    class="spotlight"></a>
+
+                     <a href="https://twitter.com/atlantis0x" class="spotlight"></a>
+
+<a href="https://discord.com/invite/j43Ycp72uK" class="spotlight"></a>
+
+                */
